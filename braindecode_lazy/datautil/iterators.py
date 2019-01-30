b76a39a1a29a73d7b1132ccc6f150f103b60354e
Lukas Alexander Wilhelm Gemein <gemeinl@informatik.uni-freiburg.de>
2019-01-30 17:23:02 +0100

    removed sampler again

--- braindecode_lazy/datautil/iterators.py
+++ braindecode_lazy/datautil/iterators.py
@@ -1,10 +1,12 @@
 from torch.utils.data import DataLoader
-from torch.utils.data.sampler import Sampler
 from numpy.random import RandomState
 import numpy as np
+import logging
 
 from braindecode.datautil.iterators import _compute_start_stop_block_inds, \
     get_balanced_batches
+
+log = logging.getLogger(__name__)
 
 
 def custom_collate(batch):
@@ -27,6 +29,7 @@ class LoadCropsFromTrialsIterator(object):
     when the data is actually needed. Reduces overall RAM requirements.
     """
     def __init__(self, input_time_length, n_preds_per_input, batch_size,
+                 check_preds_smaller_trial_len=True,
                  seed=328774, num_workers=0, collate_fn=custom_collate):
         self.batch_size = batch_size
         self.seed = seed
@@ -35,62 +38,51 @@ class LoadCropsFromTrialsIterator(object):
         self.n_preds_per_input = n_preds_per_input
         self.num_workers = num_workers
         self.collate_fn = collate_fn
+        self.check_preds_smaller_trial_len = check_preds_smaller_trial_len
 
     def reset_rng(self):
         self.rng = RandomState(self.seed)
 
     def get_n_updates_per_epoch(self, dataset, shuffle):
-        bach_sampler = mySampler(dataset, self.input_time_length,
-                                 self.batch_size, shuffle,
-                                 self.n_preds_per_input, self.rng)
-        return len(bach_sampler)
+        batch_indeces = self._get_batches(dataset=dataset, shuffle=shuffle)
+        return len(batch_indeces)
 
     def get_batches(self, dataset, shuffle):
-        bach_sampler = mySampler(dataset, self.input_time_length,
-                                 self.batch_size, shuffle,
-                                 self.n_preds_per_input, self.rng)
-        data_loader = DataLoader(dataset=dataset, batch_sampler=bach_sampler,
+        batch_indeces = self._get_batches(dataset=dataset, shuffle=shuffle)
+        data_loader = DataLoader(dataset=dataset, batch_sampler=batch_indeces,
                                  num_workers=self.num_workers, pin_memory=False,
                                  collate_fn=self.collate_fn)
         return data_loader
 
-
-class mySampler(Sampler):
-    def __init__(self, dataset, input_time_length, batch_size, shuffle, n_preds_per_input, rng):
+    def _get_batches(self, dataset, shuffle):
         # start always at first predictable sample, so
         # start at end of receptive field
-        n_receptive_field = input_time_length - n_preds_per_input + 1
+        n_receptive_field = self.input_time_length - self.n_preds_per_input + 1
         i_trial_starts = [n_receptive_field - 1] * len(dataset.X)
         i_trial_stops = [trial.shape[1] for trial in dataset.X]
 
         # Check whether input lengths ok
         input_lens = i_trial_stops
         for i_trial, input_len in enumerate(input_lens):
-            assert input_len >= input_time_length, (
+            assert input_len >= self.input_time_length, (
                 "Input length {:d} of trial {:d} is smaller than the "
                 "input time length {:d}".format(input_len, i_trial,
-                                                input_time_length))
+                                                self.input_time_length))
 
         start_stop_blocks_per_trial = _compute_start_stop_block_inds(
-            i_trial_starts, i_trial_stops, input_time_length,
-            n_preds_per_input, check_preds_smaller_trial_len=True)
+            i_trial_starts, i_trial_stops, self.input_time_length,
+            self.n_preds_per_input,
+            check_preds_smaller_trial_len=self.check_preds_smaller_trial_len)
         for i_trial, trial_blocks in enumerate(start_stop_blocks_per_trial):
             assert trial_blocks[0][0] == 0
             assert trial_blocks[-1][1] == i_trial_stops[i_trial]
 
         i_trial_start_stop_block = np.array([
             (i_trial, start, stop) for i_trial, block in
-            enumerate(start_stop_blocks_per_trial) for (start, stop) in block])
+            enumerate(start_stop_blocks_per_trial) for start, stop in block])
 
         batches = get_balanced_batches(
-            n_trials=len(i_trial_start_stop_block), rng=rng,
-            shuffle=shuffle, batch_size=batch_size)
+            n_trials=len(i_trial_start_stop_block), rng=self.rng,
+            shuffle=shuffle, batch_size=self.batch_size)
 
-        self.batchs = [i_trial_start_stop_block[batch_ind] for batch_ind in batches]
-
-    def __iter__(self):
-        for batch in self.batchs:
-            yield batch
-
-    def __len__(self):
-        len(self.batchs)
+        return [i_trial_start_stop_block[batch_ind] for batch_ind in batches]
