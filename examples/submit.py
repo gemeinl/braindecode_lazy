8db3ebc4b09d91b58a209832f8ec3c61794d1ba3
Lukas Alexander Wilhelm Gemein <gemeinl@informatik.uni-freiburg.de>
2019-03-29 16:33:26 +0100

    now submitting proper array jobs. set start and stop by cmd arg. seed is then set from within python script matching array task id

--- examples/submit.py
+++ examples/submit.py
@@ -10,7 +10,7 @@ from utils import parse_submit_args
 def dict_to_cmd_args(d):
     cmd_args = []
     for key, value in sorted(d.items()):
-        if pd.isna(value):
+        if pd.isna(value):  # or value == "None" or value is None:
             continue
         cmd_args.append("--"+key+"="+str(value))
     return " ".join(cmd_args)
@@ -25,7 +25,8 @@ def read_job_id_from_job_name(job_name):
     return dependency_job_id
 
 
-def main(configs_file, conda_env_name, python_file, queue, python_path):
+def main(configs_file, conda_env_name, python_file, queue, python_path, start,
+         stop):
     # create jobs file text in this script from which a temporary file will
     # be created
     job_file = (
@@ -47,32 +48,31 @@ def main(configs_file, conda_env_name, python_file, queue, python_path):
                    'conda activate {}'.format(conda_env_name))
     # create a tmp job file to be run
     script_name = "/home/gemeinl/jobs/slurm/run_tmp.sh"
-    batch_submit = "sbatch -p {} -c {} --array={}-{} --job-name={} {}"
-    # --time=12:00:00"
+    batch_submit = "sbatch -p {} -c {}"
+    if start is not None and stop is not None:
+        batch_submit += " --array={}-{}"
+    batch_submit += " --job-name={} {}"
+    # add n_parallel? 0-5:5
+    # add time limit? --time=12:00:00"
 
     # sbatch -p meta_gpu-ti -w metagpub -c 4 jobs/slurmbjob.pbs
-
-    # loop through all the configs
     for setting_i, (setting_name, setting) in enumerate(configs_df.iterrows()):
         config = configs_df.loc[setting_name].to_dict()
-        # TODO: what if n_folds not in config?
-        n = int(config["n_folds"])
-        num_workers = int(config["num_workers"])
-        # create a tmp job file / job for every repetition / fold
-        for j in range(n):
-            config["seed"] = j
-            cmd_args = dict_to_cmd_args(config)
-            curr_job_file = job_file.format(python_path, virtual_env,
-                                            python_file, cmd_args)
+        # n = int(config["n_folds"])
+        if "num_workers" in config:
+            num_workers = int(config["num_workers"])
+        else:
+            num_workers = 1
+        cmd_args = dict_to_cmd_args(config)
+        curr_job_file = job_file.format(python_path, virtual_env, python_file,
+                                        cmd_args)
+        with open(script_name, "w") as f:
+            f.writelines(curr_job_file)
 
-            # write tmp job file and submit it to slurm
-            with open(script_name, "w") as f:
-                f.writelines(curr_job_file)
-
-            command = batch_submit.format(
-                queue, num_workers, j, j, setting_name+str(j), script_name)
-            print(command)
-            os.system(command)
+        command = batch_submit.format(
+            queue, num_workers, start, stop, setting_name, script_name)
+        print(command)
+        os.system(command)
 
 
 if __name__ == '__main__':
