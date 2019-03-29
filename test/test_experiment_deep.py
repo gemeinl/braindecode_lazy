updated tests. they failed, since order of cv has changed. however, both versions (traditional loading and lazy loading) computed the same, incorrect results. -> they are doing the same, but due to cv change not expected result anymore. updated expected result.

test/test_experiment_deep.py
@@ -22,6 +22,8 @@ def test_run_deep_pathological():
         "eval_folder": None,
         "lazy_loading": False,
         "result_folder": None,
+        "run_on_normals": False,
+        "run_on_abnormals": False,
 
         # model specific
         "model_name": "deep",
@@ -41,32 +43,38 @@ def test_run_deep_pathological():
 
     np.testing.assert_allclose(
         np.array(exp.epochs_df.train_loss),
-        np.array([1.3472894430160522, 133.92396545410156, 15.145020484924316]),
+        # np.array([1.3472894430160522, 133.92396545410156, 15.145020484924316]),
+        np.array([1.5475528240203857, 1.6970766458790365e-10, 0.5102109313011169]),
         atol=1e-3, rtol=1e-3)
 
     np.testing.assert_allclose(
         np.array(exp.epochs_df.test_loss),
-        np.array([1.7500500679016113, 0.0, 0.0]),
+        # np.array([1.7500500679016113, 0.0, 0.0]),
+        np.array([0.9489967823028564, 72.83006286621094, 3.852987051010132]),
         atol=1e-3, rtol=1e-3)
 
     np.testing.assert_allclose(
         np.array(exp.epochs_df.train_sample_misclass),
-        np.array([0.4353356481481482, 0.25, 0.26443287037037033]),
+        np.array([0.4539236111111111, 0.0, 0.21736111111111112]),
+        # np.array([0.4353356481481482, 0.25, 0.26443287037037033]),
         atol=1e-3, rtol=1e-3)
 
     np.testing.assert_allclose(
         np.array(exp.epochs_df.test_sample_misclass),
-        np.array([0.43921296296296297, 0.0, 0.0]),
+        np.array([0.3648611111111111, 1.0, 0.6801851851851852]),
+        # np.array([0.43921296296296297, 0.0, 0.0]),
         atol=1e-3, rtol=1e-3)
 
     np.testing.assert_allclose(
         np.array(exp.epochs_df.train_misclass),
-        np.array([0.25, 0.25, 0.25]),
+        np.array([0.25, 0.0, 0.0]),
+        # np.array([0.25, 0.25, 0.25]),
         atol=1e-3, rtol=1e-3)
 
     np.testing.assert_allclose(
         np.array(exp.epochs_df.test_misclass),
-        np.array([0.0, 0.0, 0.0]),
+        np.array([0.0, 1.0, 1.0]),
+        # np.array([0.0, 0.0, 0.0]),
         atol=1e-3, rtol=1e-3)
 
 
@@ -85,6 +93,8 @@ def test_run_deep_gender():
         "eval_folder": None,
         "lazy_loading": False,
         "result_folder": None,
+        "run_on_normals": False,
+        "run_on_abnormals": False,
 
         # model specific
         "model_name": "deep",
@@ -104,30 +114,36 @@ def test_run_deep_gender():
 
     np.testing.assert_allclose(
         np.array(exp.epochs_df.train_loss),
-        np.array([0.9724488258361816, 621.3842163085938, 233.0751190185547]),
+        np.array([1.4983415603637695, 312.3372802734375, 180.93812561035156]),
+        # np.array([0.9724488258361816, 621.3842163085938, 233.0751190185547]),
         atol=1e-3, rtol=1e-3)
 
     np.testing.assert_allclose(
         np.array(exp.epochs_df.test_loss),
-        np.array([3.381948232650757, 0.0, 0.0]),
+        np.array([1.2783774137496948, 627.1044311523438, 319.7020568847656]),
+        # np.array([3.381948232650757, 0.0, 0.0]),
         atol=1e-3, rtol=1e-3)
 
     np.testing.assert_allclose(
         np.array(exp.epochs_df.train_sample_misclass),
-        np.array([0.4196643518518518, 0.75, 0.75]),
+        np.array([0.40107638888888886, 0.5, 0.4961226851851852]),
+        # np.array([0.4196643518518518, 0.75, 0.75]),
         atol=1e-3, rtol=1e-3)
 
     np.testing.assert_allclose(
         np.array(exp.epochs_df.test_sample_misclass),
-        np.array([0.560787037037037, 0.0, 0.0]),
+        np.array([0.6351388888888889, 1.0, 1.0]),
+        # np.array([0.560787037037037, 0.0, 0.0]),
         atol=1e-3, rtol=1e-3)
 
     np.testing.assert_allclose(
         np.array(exp.epochs_df.train_misclass),
-        np.array([0.25, 0.75, 0.75]),
+        np.array([0.25, 0.5, 0.5]),
+        # np.array([0.25, 0.75, 0.75]),
         atol=1e-3, rtol=1e-3)
 
     np.testing.assert_allclose(
         np.array(exp.epochs_df.test_misclass),
-        np.array([1.0, 0.0, 0.0]),
+        np.array([1.0, 1.0, 1.0]),
+        # np.array([1.0, 0.0, 0.0]),
         atol=1e-3, rtol=1e-3)

test/test_experiment_shallow_lazy.py
@@ -21,6 +21,8 @@ def test_run_shallow_pathological():
         "eval_folder": None,
         "lazy_loading": True,
         "result_folder": None,
+        "run_on_normals": False,
+        "run_on_abnormals": False,
 
         # model specific
         "model_name": "shallow",
@@ -42,32 +44,38 @@ def test_run_shallow_pathological():
 
     np.testing.assert_allclose(
         np.array(exp.epochs_df.train_loss),
-        np.array([5.638560771942139, 0.9429671168327332, 0.9720389246940613]),
+        np.array([9.553414344787598, 5.065576553344727, 3.09649920463562]),
+        # np.array([5.638560771942139, 0.9429671168327332, 0.9720389246940613]),
         atol=1e-3, rtol=1e-3)
 
     np.testing.assert_allclose(
         np.array(exp.epochs_df.test_loss),
-        np.array([15.661099433898926, 5.816369533538818, 6.5150346755981445]),
+        np.array([0.0016847262158989906, 0.8505358695983887, 3.2106971740722656]),
+        # np.array([15.661099433898926, 5.816369533538818, 6.5150346755981445]),
         atol=1e-3, rtol=1e-3)
 
     np.testing.assert_allclose(
         np.array(exp.epochs_df.train_sample_misclass),
-        np.array([0.7478788019287834, 0.450447422106825, 0.44304061572700293]),
+        np.array([0.9978788019287834, 0.708839484421365, 0.5624304525222552]),
+        # np.array([0.7478788019287834, 0.450447422106825, 0.44304061572700293]),
         atol=1e-3, rtol=1e-3)
 
     np.testing.assert_allclose(
         np.array(exp.epochs_df.test_sample_misclass),
-        np.array([1.0, 1.0, 1.0]),
+        np.array([0.0, 0.5533197329376854, 0.9093564540059347]),
+        # np.array([1.0, 1.0, 1.0]),
         atol=1e-3, rtol=1e-3)
 
     np.testing.assert_allclose(
         np.array(exp.epochs_df.train_misclass),
-        np.array([0.75, 0.5, 0.5]),
+        np.array([1.0, 0.75, 0.5]),
+        # np.array([0.75, 0.5, 0.5]),
         atol=1e-3, rtol=1e-3)
 
     np.testing.assert_allclose(
         np.array(exp.epochs_df.test_misclass),
-        np.array([1.0, 1.0, 1.0]),
+        np.array([0.0, 0.0, 1.0]),
+        # np.array([1.0, 1.0, 1.0]),
         atol=1e-3, rtol=1e-3)
 
 
@@ -86,6 +94,8 @@ def test_run_shallow_gender():
         "eval_folder": None,
         "lazy_loading": True,
         "result_folder": None,
+        "run_on_normals": False,
+        "run_on_abnormals": False,
 
         # model specific
         "model_name": "shallow",
@@ -107,30 +117,36 @@ def test_run_shallow_gender():
 
     np.testing.assert_allclose(
         np.array(exp.epochs_df.train_loss),
-        np.array([4.757225513458252, 2.666571617126465, 1.1629936695098877]),
+        np.array([2.992706298828125, 0.3431023359298706, 0.22393958270549774]),
+        # np.array([4.757225513458252, 2.666571617126465, 1.1629936695098877]),
         atol=1e-3, rtol=1e-3)
 
     np.testing.assert_allclose(
         np.array(exp.epochs_df.test_loss),
-        np.array([3.6769267808267614e-07, 2.169014123865054e-07, 2.007234570555738e-06]),
+        np.array([7.058078289031982, 0.8440737724304199, 0.5119022130966187]),
+        # np.array([3.6769267808267614e-07, 2.169014123865054e-07, 2.007234570555738e-06]),
         atol=1e-3, rtol=1e-3)
 
     np.testing.assert_allclose(
         np.array(exp.epochs_df.train_sample_misclass),
-        np.array([0.7478788019287834, 0.5806750741839762, 0.35107566765578635]),
+        np.array([0.4978788019287834, 0.1240495178041543, 0.07720929154302669]),
+        # np.array([0.7478788019287834, 0.5806750741839762, 0.35107566765578635]),
         atol=1e-3, rtol=1e-3)
 
     np.testing.assert_allclose(
         np.array(exp.epochs_df.test_sample_misclass),
-        np.array([0.0, 0.0, 0.0]),
+        np.array([1.0, 0.48891876854599403, 0.18527448071216612]),
+        # np.array([0.0, 0.0, 0.0]),
         atol=1e-3, rtol=1e-3)
 
     np.testing.assert_allclose(
         np.array(exp.epochs_df.train_misclass),
-        np.array([0.75, 0.75, 0.25]),
+        np.array([0.5, 0.0, 0.0]),
+        # np.array([0.75, 0.75, 0.25]),
         atol=1e-3, rtol=1e-3)
 
     np.testing.assert_allclose(
         np.array(exp.epochs_df.test_misclass),
-        np.array([0.0, 0.0, 0.0]),
+        np.array([1.0, 1.0, 0.0]),
+        # np.array([0.0, 0.0, 0.0]),
         atol=1e-3, rtol=1e-3)